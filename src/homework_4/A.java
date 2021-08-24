16291789d188875083567582c35a9748ab807a15
Данияр <daniyar.baiganov@gmail.com>
2021-08-24 20:26:25 +0300

    init

---
+++ src/homework_4/A.java
@@ -0,0 +1,32 @@
+package homework_4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class A {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Map<String, String> mapA = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            String[] arr = br.readLine().split(" ");
+            mapA.put(arr[0], arr[1]);
+        }
+        String input = br.readLine();
+        String result = "";
+        Set<String> set = mapA.keySet();
+        for (String key : set) {
+            if (input.equals(key)) {
+                result = mapA.get(key);
+            } else if (input.equals(mapA.get(key))) {
+                result = key;
+            }
+         }
+        System.out.println(result);
+    }
+}
---
+++ src/homework_4/D.java
@@ -0,0 +1,36 @@
+package homework_4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class D {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        Map<Integer, Integer> map = new HashMap<>();
+        String[] items = reader.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            map.put(i + 1, Integer.parseInt(items[i]));
+        }
+
+        int m = Integer.parseInt(reader.readLine());
+        items = reader.readLine().split(" ");
+        for (int i = 0; i < m; i++) {
+            int key = Integer.parseInt(items[i]);
+            map.put(key, map.get(key) - 1);
+        }
+
+        for (int key : map.keySet()) {
+            int value = map.get(key);
+            if (value >= 0) {
+                System.out.println("NO");
+            } else {
+                System.out.println("YES");
+            }
+        }
+    }
+}
